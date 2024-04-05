1dda00a30187804daf01d2d4740d189f2a8bce85
Gabriel Bittencourt Camillo de Souza <86302613+GabrielBitten@users.noreply.github.com>
2024-04-05 16:14:56 +0000

    finalizado

--- script.js
+++ script.js
@@ -3,27 +3,20 @@ const urlParams = new URLSearchParams(window.location.search);
 const evolucao= urlParams.get('evolucao');
 
 if (evolucao) {
-    const header = document.getElementById('header');
-    switch (evolucao) {
-        case 'squirtle':
-            header.querySelector('h1').textContent = 'Squirtle';
-            break;
-        case 'wartortle':
-            header.querySelector('h1').textContent = 'Wartortle';
-            break;
-        case 'blastoise':
-            header.querySelector('h1').textContent = 'Blastoise';
-            break;
-        default:
-            header.querySelector('h1').textContent = 'Pokémon Desconhecido';
-    }
     fetch(`https://pokeapi.co/api/v2/pokemon/${evolucao}`)
         .then(response => response.json())
         .then(data => {
-            console.log('Dados do Pokémon:', data);
-      
+            
+
+            const header = document.getElementById('header');
+            header.querySelector('h1').textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+
             const pokemonImage = document.getElementById('pokemonImage');
-            pokemonImage.innerHTML = `<img src="${data.sprites.other['official-artwork'].front_default}" alt="${evolucao}">`;
+            pokemonImage.innerHTML = `<img src="${data.sprites.front_default}" alt="${evolucao}">`;
+
+            novaImagem.src = data.sprites.front_default; 
+            novaImagem.alt = evolucao; 
+            document.body.appendChild(novaImagem);
         })
-        .catch(error => console.error('Erro ao recuperar os dados:', error));
+        .catch(error => console.error('Erro ao recuperar  dados:', error));
 }
